db5e8275bf8fc435a7fd6c3a2162b1f2b9fdc19b
iovasebastian <iovasebastian@yahoo.com>
2025-01-07 15:38:48 +0200

    added vercel speed insights

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { HashRouter } from 'react-router-dom';
 import Login from './components/login';
 import Main from './components/main';
 import Register from './components/register';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <SpeedInsights/>
       <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
