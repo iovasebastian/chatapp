638685cfb089617bcb2bef30f6bd97b397e55f59
iovasebastian <iovasebastian@yahoo.com>
2025-01-08 22:29:11 +0200

    last message

--- src/components/chatline.jsx
+++ src/components/chatline.jsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from 'react';
 import '../style/chatline.css';
 const Chatline = (props) =>{
     const friendshipId = props.friendshipId;
-    const handleMessages = () =>{
-        
+    const [lastMessage, setLastMessage] = useState("");
+    const handleMessages = () =>{      
         localStorage.setItem("chatId", friendshipId);
     } 
     const handleDelete = async () =>{
@@ -24,10 +25,40 @@ const Chatline = (props) =>{
               console.error(error);
             }
     }
+    const fetchMessages = async (friendshipId) => {
+      try {  
+        if(friendshipId!='undefined'){
+          const response = await fetch("https://chatapp-server-ghz3.onrender.com/getmessages.php", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ chatId: friendshipId }),
+          });
+        
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          const data = await response.json();
+      
+          if (data.status === "success") {
+            setLastMessage(data.messages[data.messages.length-1].body || []);
+            console.log('last message', data.messages[data.messages.length-1].body);
+            console.log('messages',data.messages)
+          } else {
+            console.error("Error from server:", data.message);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching messages:", error);
+      }
+    };
+    useEffect(()=>{
+      fetchMessages(friendshipId);
+    },[])
+
     return(
         <div className="divChatLine" onClick={handleMessages}>
             <h3>{props.name}</h3>
-            <p>{props.lastMessage}</p>
+            <p>{lastMessage || "No messages yet"}</p>
             <button onClick={handleDelete}>Delete friendship</button>
         </div>
     )
